f32d098737dd8b9f63d30dbfc9c4432073045e52
prerna-123456 <sutharprerna806@gmail.com>
2025-04-22 16:53:29 +0530

    Muje ghar jaana

---
+++ SimpleCalculator.java
@@ -0,0 +1,19 @@
+package JAVA;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        int addition = a + b;
+        int subtraction = a - b;
+        int multiplication = a * b;
+        int division = a / b;
+
+        System.out.println("Addition: " + addition);
+        System.out.println("Subtraction: " + subtraction);
+        System.out.println("Multiplication: " + multiplication);
+        System.out.println("Division: " + division);
+    }
+}
+
