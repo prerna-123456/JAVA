7d6b61bff19010418ffa8ac251a92a2263691d61
prerna-123456 <sutharprerna806@gmail.com>
2025-04-22 15:18:14 +0530

    Bore ho reha hai

---
+++ EvenOrOdd.java
@@ -0,0 +1,14 @@
+package JAVA;
+
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        int n = 15;
+
+        if (n % 2 == 0) {
+            System.out.println("Checking if the number is even or odd...");
+            System.out.println(n + " is even.");
+        } else {
+            System.out.println(n + " is odd.");
+        }
+    }
+}
