f3053725887a2c7dd28f8ecc73f00f9f57d23621
prerna-123456 <sutharprerna806@gmail.com>
2025-04-22 16:51:27 +0530

    Muje ghar jaana hai

---
+++ Fibonacci.java
@@ -0,0 +1,18 @@
+package JAVA;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n = 10; // number of terms
+        int first = 0, second = 1;
+        
+        System.out.println("Fibonacci Series up to " + n + " terms:");
+        
+        for (int i = 1; i <= n; i++) {
+            System.out.print(first + " ");
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+}
+
