7f62b42168e61fbe87a8c3548a78b04c68da8107
prerna-123456 <sutharprerna806@gmail.com>
2025-04-22 16:02:43 +0530

    Muje bahar jaana

---
+++ Addition.java
@@ -0,0 +1,10 @@
+package JAVA;
+
+public class Addition {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        int sum = a + b;
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+    }
+}
