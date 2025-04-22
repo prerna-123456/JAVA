92931e76c114d6f87c2d37d19e07aaef159773bb
prerna-123456 <sutharprerna806@gmail.com>
2025-04-22 16:47:21 +0530

    Muje bahar

---
+++ Multiplication.java
@@ -0,0 +1,10 @@
+package JAVA;
+
+public class Multiplication {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 6;
+        int product = a * b;
+        System.out.println("Product of " + a + " and " + b + " is: " + product);
+    }
+}
