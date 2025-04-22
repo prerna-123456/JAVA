3876aa9f5763a097e9097828219f53264f100f88
prerna-123456 <sutharprerna806@gmail.com>
2025-04-22 16:49:39 +0530

    Muje

---
+++ LargestNumber.java
@@ -0,0 +1,17 @@
+package JAVA;
+
+public class LargestNumber {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        if (a > b) {
+            System.out.println(a + " is the largest.");
+        } else if (a < b) {
+            System.out.println(b + " is the largest.");
+        } else {
+            System.out.println("Both numbers are equal.");
+        }
+    }
+}
+
